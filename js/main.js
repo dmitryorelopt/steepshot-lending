531b1c2e41d685bd5f89fbe329b5d51299e4be9c
dmitryorelopt <dmitryorelopt@gmail.com>
2018-08-09 12:57:49 +0300

    added mouseWheel listener

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
-window.onload = () => {
-	$(window).click((e) => {
+const $window = $(window);
+const pages = [
+	'current_index',
+	'current_description'
+];
+let loading = false;
+let currentPage = 0;
+
+$window.ready(() => {
+	const main = $('.main');
+	const swapTime = parseInt($(':root').css('--swap-time'), 10);
+
+	$window.resize(function () {
+		window.setTimeout('location.reload()', 200);
+	});
+
+	$window.bind('mousewheel', (e) => {
 		e.stopPropagation();
-		const mainContainers = $('.main > div');
-		let left = mainContainers.css('left');
-		mainContainers.css('left', () => {
-			return (-1 * parseFloat(left)) + 'px';
-		})
-	})
-};
+		if (loading) {
+			return;
+		}
+		loading = true;
+
+		if(e.originalEvent.wheelDelta > 0) {
+			if (currentPage > 0) {
+				main.removeClass(pages[currentPage]);
+				currentPage--;
+				main.addClass(pages[currentPage]);
+			}
+		} else {
+			if (currentPage < pages.length - 1) {
+				main.removeClass(pages[currentPage]);
+				currentPage++;
+				main.addClass(pages[currentPage]);
+			}
+		}
+
+		setTimeout(() => {
+			loading = false;
+		}, swapTime)
+	});
+
+});
